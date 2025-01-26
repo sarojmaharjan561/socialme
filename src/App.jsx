7123b327a656f5f0e718631e232d8e7d21cdc37d
Saroj Maharjan <saroj.maharjan.561@gmail.com>
2025-01-27 09:23:59 +1100

    added sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 
 function App() {
 	//   const [count, setCount] = useState(0)
 
 	return (
-		<>
-			<Header></Header>
-			<Footer></Footer>
-		</>
+		<div className="app-container">
+			<Sidebar></Sidebar>
+			<div className="content">
+				<Header></Header>
+				<Footer></Footer>
+			</div>
+		</div>
 	);
 }
 
